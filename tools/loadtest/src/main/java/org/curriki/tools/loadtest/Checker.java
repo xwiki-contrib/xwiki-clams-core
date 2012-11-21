df3068cef3bc4b0bb514fed012521aff093f39a6
Paul Libbrecht <paul@hoplahup.net>
2012-11-21 23:48:54 +0100

    load-tester now somewhat trustable. Doesn't delete, unfortunately, so that accumulation in the page history may happen. (delete triggers a hibernate exception at next save). :CURRIKI-5963: cluster-consistency checker paul

--- tools/loadtest/src/main/java/org/curriki/tools/loadtest/Checker.java
+++ tools/loadtest/src/main/java/org/curriki/tools/loadtest/Checker.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.jdom2.*;
+import org.jdom2.filter.ContentFilter;
 import org.jdom2.filter.ElementFilter;
 import org.jdom2.filter.Filter;
 import org.jdom2.input.SAXBuilder;
@@ -18,40 +19,59 @@ import java.util.Iterator;
 class Checker {
 
     private static Log LOG = LogFactory.getLog(Checker.class);
+    static Namespace HTMLNS = Namespace.getNamespace("http://www.w3.org/1999/xhtml");
 
     static Document parseResponse(HttpResponse x) throws IOException, JDOMException {
 
         String content = org.apache.commons.io.IOUtils.toString(x.getEntity().getContent(), "utf-8");
         content = content.replaceAll("curriki:", "").replaceAll("Curriki:","");
+        //System.out.println("Content is " + content);
 
         SAXBuilder builder = new SAXBuilder("org.cyberneko.html.parsers.SAXParser");
         builder.setProperty("http://cyberneko.org/html/properties/default-encoding","UTF-8");
         return builder.build(new StringReader(content));
     }
 
-    static void checkResponseHas(String elementName, String id, Document doc, String mustFind) {
+    static void checkDocumentHas(String elementName, String id, Document doc, String mustFind) {
         Element found = null;
-        Filter filter = new ElementFilter("TEXTAREA");
+        Filter filter = new ElementFilter(elementName, HTMLNS);
         Iterator<Content> it = doc.getRootElement().getDescendants(filter);
         while(it.hasNext()) {
             Element elt = (Element) it.next();
-            if(id.equals(elt.getAttributeValue("id"))) {
+            if(id==null || id.equals(elt.getAttributeValue("id"))) {
                 found = elt; break;
             }
         }
-        String value = found.getTextNormalize();
+
+        // compute descendant texts
+        StringBuilder builder = new StringBuilder();
+        it = found.getDescendants(new ContentFilter(ContentFilter.TEXT));
+        while(it.hasNext()) {
+            Text t = (Text) it.next();
+            builder.append(t.getTextNormalize());
+        }
+        String value = builder.toString();
         if(value==null)
             throw new IllegalStateException("No element following pattern \"" + elementName + "\" with id \""+id+"\" has been found.");
         else if(mustFind.equals(value)) {
             // nothing to do
             LOG.info("----  Expected text is found.");
         } else {
-            throw new IllegalStateException("Element's text differs." );
+            throw new IllegalStateException("Element's text differs: \n  Expected: \"" + mustFind + "\"\n  But found: \"" + value + "\"." );
         }
     }
 
     static void assertResponseIsRedirect(HttpResponse x, String location) {
-        if(x.getStatusLine().getStatusCode()!=302) throw new IllegalStateException("Did not receive a redirect!");
+        if(x.getStatusLine().getStatusCode() == 200) {
+            LOG.warn("Received 200: ");
+            try {
+                x.getEntity().writeTo(System.out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if(x.getStatusLine().getStatusCode()!=302) throw new IllegalStateException("Did not receive a redirect! (Status line : " + x.getStatusLine() + "");
         Header[] headers = x.getHeaders("Location");
         if(headers.length!=1) throw new IllegalStateException("Was expecting one Location header!");
         if(location.equals(headers[0].getValue())) {
--- tools/loadtest/src/main/java/org/curriki/tools/loadtest/TestClusteringWorksOnTitles.java
+++ tools/loadtest/src/main/java/org/curriki/tools/loadtest/TestClusteringWorksOnTitles.java
@@ -4,6 +4,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpResponse;
 import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.filter.ElementFilter;
 
 import java.io.IOException;
 
@@ -39,6 +41,7 @@ public class TestClusteringWorksOnTitles {
     private String baseURL;
     private String userName, password;
     private String node1, node2;
+    private String page = "Sandbox/TempForTestingClustering";
     private XWikiHttpClient client;
 
     private void init() throws IOException {
@@ -48,39 +51,68 @@ public class TestClusteringWorksOnTitles {
 
     private void run() throws Exception {
         HttpResponse response;
+        Document doc;
 
         client.tryLogin();
         LOG.info("--- Now on " + client.getClusterNodeOfSession());
-        String newContent = "Title Edited On " + client.getClusterNodeOfSession() + " " + ((int) (Math.random()*10000));
-        LOG.info("-- Uploading \"" + newContent + "\" to Sandbox/X.");
-        response = client.postForm(baseURL + "xwiki/bin/save/Sandbox/X", "title:" + newContent, "content:" + newContent, "comment:edited");
-        Checker.assertResponseIsRedirect(response, baseURL + "xwiki/bin/view/Sandbox/X");
 
-        LOG.info("-- Checking it arrived on other cluster");
+        /*
+        // disabled: check and deletion
+        // get first to verify if there
+        response = client.getPage(baseURL + "xwiki/bin/view/" + page + "?language=en");
+        if(response.getStatusLine().getStatusCode()==200) {
+            // delete first
+            LOG.info("--- deleting earlier versions.");
+            response = client.getPage(baseURL + "xwiki/bin/delete/"+page+"?confirm=1&language=en");
+            doc = Checker.parseResponse(response);
+            Checker.checkDocumentHas("LEGEND", null, doc, "Delete");
+            for(Element elt: doc.getRootElement().getDescendants(new ElementFilter("A",Checker.HTMLNS))) {
+                String href = elt.getAttributeValue("href");
+                if(href!=null && href.startsWith("/xwiki/bin/delete/" + page) && href.contains("id=")) {
+                    LOG.info("---- deleting: " + href);
+                    client.getPage(baseURL + href.substring(1));
+                }
+            }
+        } else {
+            LOG.info("--- earlier version not found.");
+        }
+        Thread.sleep(10000);
+        */
+
+
+        String newContent = "Title Edited On " + client.getClusterNodeOfSession() + " " + ((int) (Math.random()*10000));
+        LOG.info("-- Uploading \"" + newContent + "\" to "+ page +".");
+        response = client.postForm(baseURL + "xwiki/bin/save/" + page + "?language=en", "title:" + newContent, "content:" + newContent, "comment:edited");
+        Checker.assertResponseIsRedirect(response, baseURL + "xwiki/bin/view/" + page);
+        Thread.sleep(10000);
+
+        LOG.info("-- Checking it arrived on other cluster node.");
         client.changeClusterNode(node1, node2);
         LOG.info("--- Now on " + client.getClusterNodeOfSession());
-        response = client.getPage(baseURL + "xwiki/bin/edit/Sandbox/X");
-        Document doc = Checker.parseResponse(response);
-        Checker.checkResponseHas("TEXTAREA", "content", doc, newContent); // [@name='content']
+        response = client.getPage(baseURL + "xwiki/bin/edit/" + page);
+        doc = Checker.parseResponse(response);
+        Checker.checkDocumentHas("TEXTAREA", "content", doc, newContent); // [@name='content']
         LOG.info("-- It did arrive.");
 
 
 
+        Thread.sleep(10000);
         newContent = "Title Edited On " + client.getClusterNodeOfSession() + " " + ((int) (Math.random()*10000));
         LOG.info("--- Now on " + client.getClusterNodeOfSession());
-        LOG.info("-- Uploading \"" + newContent + "\" to Sandbox/X.");
+        LOG.info("-- Uploading \"" + newContent + "\" to "+ page +".");
         client.changeClusterNode(node1, node2);
-        response = client.postForm(baseURL + "xwiki/bin/save/Sandbox/X", "title:" + newContent, "content:" + newContent, "comment:edited-for-testing");
-        Checker.assertResponseIsRedirect(response, baseURL + "xwiki/bin/view/Sandbox/X");
+        response = client.postForm(baseURL + "xwiki/bin/save/" + page + "?language=en", "title:" + newContent, "content:" + newContent, "comment:edited-for-testing");
+        Checker.assertResponseIsRedirect(response, baseURL + "xwiki/bin/view/" + page);
 
+        Thread.sleep(10000);
         client.changeClusterNode(node1, node2);
         LOG.info("-- Checking it arrived on other cluster node (\"" + client.getClusterNodeOfSession() + "\")");
         LOG.info("--- Now on " + client.getClusterNodeOfSession());
-        response = client.getPage(baseURL + "xwiki/bin/edit/Sandbox/X");
+        response = client.getPage(baseURL + "xwiki/bin/edit/" + page + "?language=en");
         doc = Checker.parseResponse(response);
-        Checker.checkResponseHas("TEXTAREA", "content", doc, newContent);
+        Checker.checkDocumentHas("TEXTAREA", "content", doc, newContent);
         LOG.info("-- It did arrive.");
-        client.getPage(baseURL + "xwiki/bin/cancel/Sandbox/X");
+        client.getPage(baseURL + "xwiki/bin/cancel/" + page + "?language=en");
         LOG.info("-- Cleaned up.");
     }
 
